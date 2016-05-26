1e593affb167e2186f091c07af4d6651f7ab6cae
Alex L <alex.redcode@gmail.com>
2016-05-25 20:40:26 -0400

    Allow for bootstrapping in non VS environment

--- src/bootstrap.ts
+++ src/bootstrap.ts
@@ -18,36 +18,50 @@ if(process.env.ENV === 'prod') {
  * our top level component that holds all of our components
  */
 import {App} from './app/app';
+if (typeof Windows !== 'undefined') {
+  // We are running the app as a UWP app
+  var app = WinJS.Application;
+  var activation = Windows.ApplicationModel.Activation;
 
-var app = WinJS.Application;
-var activation = Windows.ApplicationModel.Activation;
+  app.onactivated = function(args) {
+    if (args.detail.kind === activation.ActivationKind.launch) {
+      if (args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
 
-app.onactivated = function (args) {
-    if(args.detail.kind === activation.ActivationKind.launch) {
-        if(args.detail.previousExecutionState !== activation.ApplicationExecutionState.terminated) {
+        // The Windows app has been initialized, let's start Angular 2
+        bootstrap(App, [
+          // These are dependencies of our App
+          ...HTTP_PROVIDERS,
+          ...ROUTER_PROVIDERS,
+          ...ENV_PROVIDERS,
+          provide(LocationStrategy, { useClass: HashLocationStrategy }) // use #/ routes, remove this for HTML5 mode
+        ])
+          .catch(err => console.error(err));
 
-            // The Windows app has been initialized, let's start Angular 2
-            bootstrap(App, [
-                // These are dependencies of our App
-                ...HTTP_PROVIDERS,
-                ...ROUTER_PROVIDERS,
-                ...ENV_PROVIDERS,
-                provide(LocationStrategy, {useClass: HashLocationStrategy}) // use #/ routes, remove this for HTML5 mode
-            ])
-                .catch(err => console.error(err));
-
-        } else {
-            // TODO: cette application a été suspendue, puis terminée.
-            // Pour créer une expérience utilisateur fluide, restaurez l'état de l'application ici afin de donner l'impression que l'application n'a jamais cessé de fonctionner.
-        }
-        args.setPromise(WinJS.UI.processAll());
+      } else {
+        // TODO: cette application a été suspendue, puis terminée.
+        // Pour créer une expérience utilisateur fluide, restaurez l'état de l'application ici afin de donner l'impression que l'application n'a jamais cessé de fonctionner.
+      }
+      args.setPromise(WinJS.UI.processAll());
     }
-};
+  };
 
-app.oncheckpoint = function (args) {
+  app.oncheckpoint = function(args) {
     // TODO: cette application va être suspendue. Enregistrez ici tous les états qui doivent être conservés entre les suspensions.
     // Vous utilisez l'objet WinJS.Application.sessionState, qui est automatiquement enregistré et restauré entre les suspensions.
     // Si vous devez effectuer une opération asynchrone avant la suspension de votre application, appelez args.setPromise().
-};
+  };
 
-app.start();
+  app.start();
+
+} else {
+  // Not running UWP, proceed as normal
+  bootstrap(App, [
+    // These are dependencies of our App
+    ...HTTP_PROVIDERS,
+    ...ROUTER_PROVIDERS,
+    ...ENV_PROVIDERS,
+    provide(LocationStrategy, { useClass: HashLocationStrategy }) // use #/ routes, remove this for HTML5 mode
+  ])
+    .catch(err => console.error(err));
+
+}
